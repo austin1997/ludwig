231285e04ce0a13bb1dd809dc5f922b627694444
Austin <austin1997@sina.com>
2022-07-20 13:28:53 +0100

    add sample benchmarks

---
+++ benchmark/64x64x64/submit2-4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH --job-name=64_2_4
+#SBATCH --gres=gpu:4
+#SBATCH --time=00:30:00
+#SBATCH --nodes=2
+#SBATCH --exclusive
+#SBATCH --partition=gpu-cascade
+#SBATCH --qos=gpu
+
+#module load intel-mpi-19
+#module load intel-comiplers-19
+#module load nvidia/cuda-11.2
+NVHPC_VERSION=22.2
+module load nvidia/nvhpc/$NVHPC_VERSION
+module load intel-20.4/compilers
+module load intel-20.4/mpi
+module load mpt
+
+date
+echo $LD_LIBRARY_PATH
+
+srun --ntasks=8 --tasks-per-node=4 ../../src/Ludwig.exe ./input
+
+date
